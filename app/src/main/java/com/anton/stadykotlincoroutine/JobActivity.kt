8c6c8b4eb2771bfc8b4acf64229f64edfad93eae
Anton Voloshyn <antonvoloshyn@Antons-MacBook-Pro.local>
2021-09-23 17:19:35 +0300

    first commit

---
+++ app/src/main/java/com/anton/stadykotlincoroutine/JobActivity.kt
@@ -0,0 +1,95 @@
+package com.anton.stadykotlincoroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.coroutines.*
+
+class JobActivity : AppCompatActivity() {
+
+    private val PROGRESS_MAX = 100
+    private val PROGRESS_START = 0
+    private val TIME_JOB = 4_000
+    private lateinit var job: CompletableJob
+    private lateinit var jobButton: Button
+    private lateinit var jobProgressBar: ProgressBar
+    private lateinit var jobCompleteText: TextView
+    private val TAG = "Job Activity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job)
+        initUI()
+
+        jobButton.setOnClickListener {
+            if (!::job.isInitialized) {
+                initJob()
+            }
+            jobProgressBar.startJobOrCancel(job)
+        }
+    }
+    private fun initUI() {
+        jobButton = findViewById(R.id.job_button)
+        jobProgressBar = findViewById(R.id.job_progress_bar)
+        jobCompleteText = findViewById(R.id.job_complete_text)
+    }
+
+    fun ProgressBar.startJobOrCancel(job: Job) {
+        if (this.progress > 0) {
+            Log.d(TAG, "${job} is already active. Cancelling...")
+            jobButton.text = "Start job"
+            resetJob()
+        } else {
+            jobButton.text = "Cancel job"
+            CoroutineScope(Dispatchers.IO + job).launch {
+                Log.d(TAG, "coroutin ${this} is activaited with job")
+
+                for (i in PROGRESS_START..PROGRESS_MAX) {
+                    delay((TIME_JOB / PROGRESS_MAX).toLong())
+                    this@startJobOrCancel.progress = i
+                }
+
+                updateJobCompleteTextView("Job is complete")
+            }
+        }
+    }
+
+    private fun updateJobCompleteTextView(text: String) {
+        GlobalScope.launch(Dispatchers.Main) {
+            jobCompleteText.text = text
+        }
+    }
+
+    private fun resetJob() {
+        if (job.isActive || job.isCompleted) {
+            job.cancel(CancellationException("Resetting Job"))
+        }
+        initJob()
+    }
+
+    fun initJob() {
+        job = Job()
+        job.invokeOnCompletion {
+            it?.message.let {
+                var msg = it
+                if (msg.isNullOrBlank()) {
+                    msg = "Unknown cancellation error"
+                }
+                Log.d(TAG, "${job} was cancelled. Reason: $msg")
+                showToast(msg)
+            }
+        }
+        jobProgressBar.max = PROGRESS_MAX
+        jobProgressBar.progress = PROGRESS_START
+    }
+
+    fun showToast(text: String) {
+        GlobalScope.launch(Dispatchers.Main) {
+            Toast.makeText(this@JobActivity, text, Toast.LENGTH_LONG).show()
+        }
+    }
+}
